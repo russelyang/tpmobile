4f45f735e0d9342670946836c2f8c4e2b3633d33
russelyang <russelyang@gmail.com>
2011-09-07 11:39:34 -0700

    change to event modle

--- simple.js
+++ simple.js
@@ -1,4 +1,41 @@
-var count = 0;
-exports.next = function() {
-	return count++;
-}
+var ContactList = require("./lib/contacts-list");
+var ContactSearch = require("./lib/contacts-search");
+
+var server = {"host" : "ryang.corp.homestore.net", "port" : "80"};
+var user = {"name" : "russel.yang", "password" : "toptop"};
+var tpo = require('./lib/mobile');
+
+(function() {
+ 	console.time("login");
+	var m = new tpo.Mobile(server,user);
+	m.on('data', function(userhash) {
+			console.timeEnd("login");
+		}
+	);
+
+	var contactlist = new ContactList();
+	contactlist.on('data', function(d) {
+		console.timeEnd("contactList");
+	});
+
+	m.on('data', function(userhash) {
+			console.time("contactList");
+			contactlist.fetch(server, userhash);
+		});
+
+
+	var contactSearch = new ContactSearch();
+	contactSearch.on('data', function(d) {
+		console.timeEnd("contactSearch");
+	});
+	m.on('data', function(userhash) {
+			console.time("contactSearch");
+			contactSearch.fetch(server, userhash);
+		});
+	
+
+
+	m.login();
+})();
+
+
--- warm-up-production.js
+++ warm-up-production.js
@@ -2,5 +2,8 @@ var contactList = require("./lib/contacts-list");
 
 var server = {"host" : "ryang.corp.homestore.net", "port" : "80"};
 var user = {"name" : "russel.yang", "password" : "toptop"};
+var tpo = require('./lib/mobile');
 
-contactList(server, user);
+var m = new tpo.Mobile(server,user);
+m.on('data', contactList);
+m.login();
